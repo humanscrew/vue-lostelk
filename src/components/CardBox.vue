651ce4cfa52cb42e0485e4afe7d413b6b73fbca6
humanscrew <1084465086@qq.com>
2021-05-06 00:30:44 +0800

    CardBox compontent

---
+++ src/components/CardBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-card class="box-card" shadow="always">
+    <template #header>
+      <div class="card-header">
+        <span>卡片名称</span>
+        <el-button icon="el-icon-search" circle type="primary" plain size="small"></el-button>
+        <!-- 加载状态 => :loading="true" -->
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="box-card-text box-card-item">
+      {{ "列表内容 " + o }}
+    </div>
+  </el-card>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardBox",
+  props: {},
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang='less'>
+  .card-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+  }
+    
+  .box-card-text {
+    font-size: 14px;
+  }
+
+  .box-card-item {
+    margin-bottom: 18px;
+  }
+
+  .box-card {
+    width: auto;
+  }
+</style>
