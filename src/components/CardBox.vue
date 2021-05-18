889f5dd7f806de8337262ee6350b4415a99ebaa7
humanscrew <1084465086@qq.com>
2021-05-19 00:56:07 +0800

    gain hover css

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -1,22 +1,28 @@
 <template>
-  <el-card class="box-card" shadow="always">
-    <template #header>
-      <div class="card-header">
-        <span>卡片名称</span>
-        <el-button
-          icon="el-icon-search"
-          circle
-          type="primary"
-          plain
-          size="small"
-        ></el-button>
-        <!-- 加载状态 => :loading="true" -->
+  <div class="hvr-grow-shadow">
+    <el-card class="box-card" shadow="always">
+      <template #header>
+        <div class="card-header">
+          <span class="text-scale">卡片名称</span>
+          <el-button
+            icon="el-icon-search"
+            circle
+            type="primary"
+            plain
+            size="small"
+          ></el-button>
+          <!-- 加载状态 => :loading="true" -->
+        </div>
+      </template>
+      <div
+        v-for="o in 4"
+        :key="o"
+        class="box-card-text box-card-item text-scale"
+      >
+        {{ "列表内容 " + o }}
       </div>
-    </template>
-    <div v-for="o in 4" :key="o" class="box-card-text box-card-item">
-      {{ "列表内容 " + o }}
-    </div>
-  </el-card>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -39,16 +45,34 @@ export default defineComponent({
   justify-content: space-between;
   align-items: center;
 }
-
 .box-card-text {
-  font-size: 14px;
+  font-size: auto;
 }
-
 .box-card-item {
   margin-bottom: 18px;
 }
-
 .box-card {
   width: auto;
+}
+
+/* Grow Shadow */
+.hvr-grow-shadow {
+  // display: inline-block;
+  // vertical-align: middle;
+  // -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  // -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  // transition-timing-function: ease-in-out;
+  // -webkit-transition-property: box-shadow, transform;
+  transition-property: box-shadow, transform;
+}
+.hvr-grow-shadow:hover,
+.hvr-grow-shadow:focus,
+.hvr-grow-shadow:active {
+  box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.5);
+  // -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
