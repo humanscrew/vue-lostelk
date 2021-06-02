159614a24a0d114cb01577744ec77e266483112b
humanscrew <1084465086@qq.com>
2021-06-03 01:27:24 +0800

    aes+ras done

--- src/plugins/crypto-js/AES/index.ts
+++ src/plugins/crypto-js/AES/index.ts
@@ -18,6 +18,7 @@ export default {
         const key = CryptoJS.enc.Utf8.parse(keyStr);
         const src = CryptoJS.enc.Utf8.parse(word);
         const encrypted = CryptoJS.AES.encrypt(src, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
+        // const encrypted = CryptoJS.AES.encrypt(src, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.ZeroPadding });
         return encrypted.toString();
     },
     //解密
